ce9f3c604bfb8a10e5d913ff2b3491865b92e180
ScottWeber27 <webers04@uwosh.edu>
2022-10-31 17:00:06 -0500

    start of grid

--- lib/Memories.dart
+++ lib/Memories.dart
@@ -2,17 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'MainModel.dart';
 
-class Memories extends StatelessWidget {
+class Memories extends StatefulWidget {
   const Memories({super.key});
 
+  @override
+  State<Memories> createState() => _MemoriesState();
+}
+
+class _MemoriesState extends State<Memories> {
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Memories'),
-      ),
-      body: const Center(
-        child: Text('I remember'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('My Memories'),
+        ),
+        body: GridView.builder(
+            itemCount: 100,
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 150,
+              crossAxisSpacing: 10.0,
+              mainAxisSpacing: 10.0,
+            ),
+            itemBuilder: (context, index) => Card(
+                  child: InkResponse(
+                    child: Text("$index"),
+                    onTap: ()=> print(index),
+                  ),
+                )));
   }
 }
