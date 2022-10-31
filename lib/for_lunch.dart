703ac632acffdf0c3a76284ab3c2289fbcc443e0
mise11 <115748357+mise11@users.noreply.github.com>
2022-10-31 15:45:14 -0500

    Update for_lunch.dart

--- lib/for_lunch.dart
+++ lib/for_lunch.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'Main_Model.dart';
+import 'main_model.dart';
 
 ///list of states
 const List<String> states = [
