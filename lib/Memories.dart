eb6f65eb822f0dc7a18ee803b943336bf3840723
ScottWeber27 <webers04@uwosh.edu>
2022-10-31 20:49:21 -0500

    updated memories page

--- lib/Memories.dart
+++ lib/Memories.dart
@@ -2,30 +2,92 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'main_model.dart';
 
-class Memories extends StatefulWidget {
+class Memories extends StatelessWidget {
   const Memories({super.key});
 
-  @override
-  State<Memories> createState() => _MemoriesState();
-}
-
-class _MemoriesState extends State<Memories> {
   Widget build(BuildContext context) {
+    ///will eventually not be ard coded and pulled from data
+    List<String> memoriesListTemp = [
+      'Great Food!',
+      'Hard to find the enterance',
+      'God Bless America',
+      'The worst place I have ever eaten!',
+      'This was okay',
+      'I will never come back',
+      'I did enjoy this quite a bit'
+    ];
+    double numStars = 3.5;
+
     return Scaffold(
         appBar: AppBar(
           title: const Text('My Memories'),
         ),
         body: GridView.builder(
-            itemCount: 100,
+            itemCount: memoriesListTemp.length,
             gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
               maxCrossAxisExtent: 150,
-              crossAxisSpacing: 10.0,
-              mainAxisSpacing: 10.0,
+              childAspectRatio: .5,
             ),
+
+            ///is just a card right now but would like images to be pulled from data
             itemBuilder: (context, index) => Card(
+                  color: Colors.blue,
                   child: InkResponse(
-                    child: Text("$index"),
-                    onTap: ()=> print(index),
+                    ///will be just an image eventually, once tapping image you then get more info
+                    child: Center(
+                        child: Column(
+                      children: [
+                        Text("Memory: $index ${memoriesListTemp[index]}"),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            ///will apply the correct amount of stars currently is not pulled from db
+                            for (int i = 0; i < numStars.ceil(); i++) ...<Icon>{
+                              const Icon(Icons.star,
+                                  color: Colors.yellow, size: 30),
+                            }
+                          ],
+                        )
+                      ],
+                    )),
+                    onTap: () => showDialog(
+                        context: context,
+                        builder: ((context) {
+                          return AlertDialog(
+                            content: Stack(
+                              fit: StackFit.loose,
+                              children: [
+                                Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+
+                                    ///will eventurally have the picture, rating, date, location shown in pop up
+                                    children: [
+                                      Text(
+                                          "Memory: $index ${memoriesListTemp[index]}"),
+                                      const Text("Memory from: 10-10-2010"),
+                                      Row(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          ///will apply the correct amount of stars currently is not pulled from db
+                                          for (int i = 0;
+                                              i < numStars.ceil();
+                                              i++) ...<Icon>{
+                                            const Icon(Icons.star,
+                                                color: Colors.yellow, size: 30),
+                                          }
+                                        ],
+                                      )
+                                    ])
+                              ],
+                            ),
+                          );
+                        })),
                   ),
                 )));
   }
